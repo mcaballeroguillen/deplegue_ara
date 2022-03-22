170a5a092184bab56b4c75236c958a36b1d601af
Marco Caballero <mcaballeroguillen@gmail.com>
2022-03-22 13:43:03 -0600

    punto

--- eventcalendar/settings.py
+++ eventcalendar/settings.py
@@ -161,9 +161,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i8e1s3!_(fjsiv%1pn3sb3o=s)!p*nzwh1$gp5-l&%nb!d=y_s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['190.107.148.45']
+ALLOWED_HOSTS = ['.','190.107.148.45']
 
 
 # Application definition
